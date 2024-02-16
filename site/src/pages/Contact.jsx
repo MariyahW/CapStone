24009a03fc04fbf9ce7bd44539f034241098f214
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-02-15 20:40:29 -0500

    Contact form replicated

--- site/src/pages/Contact.jsx
+++ site/src/pages/Contact.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
-import Form from 'react-bootstrap/Form';
-
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
 
 export default function Contact() {
   return (
-    
-        <>
-          <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-          <Form.Control
-            type="password"
-            id="inputPassword5"
-            aria-describedby="passwordHelpBlock"
-          />
-          <Form.Text id="passwordHelpBlock" muted>
-            Your password must be 8-20 characters long, contain letters and numbers,
-            and must not contain spaces, special characters, or emoji.
-          </Form.Text>
-        </>
-      );
-    }
+    <div className="form">
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupEmail" required>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Phone</Form.Label>
+        <Form.Control type="tel" placeholder="Enter phone number" required />
+      </Form.Group>
 
+      <Form.Select aria-label="Default select example" required>
+        <option selected disabled>
+          Please Make A Selection
+        </option>
+        <option value="resume">Resume Inquiry</option>
+        <option value="contract">Contract Work</option>
+        <option value="other">Other</option>
+      </Form.Select>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+
+      <Button variant="secondary" size="md"type="submit">
+        Submit
+      </Button>
+      
+    </div>
+  );
+}
 
 //     <div className='form'>
 //     <form >
@@ -37,7 +55,6 @@ export default function Contact() {
 //             <option value="other">Other (Please Explain)</option>
 //         </select>
 //             <input type="text" name="" id="" />
-    
 
 //     </form>
 //     </div>
