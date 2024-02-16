eb7301e464ba8dce4f1546b11cece268e2f30130
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-02-16 10:57:26 -0500

    Updated background image to be centered and no-repeat

--- site/src/components/Image.jsx
+++ site/src/components/Image.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import bg from "../visuals/coding_image.jpg";
-import photo from "../visuals/imageOfSelf.jpg"
+
 export default function Image() {
   return (
    
- <div  style= {{  backgroundImage: `url(${bg})`, width:'100%', filter:'blur(4px)', height:'500px' }}
+ <div  style= {{  backgroundImage: `url(${bg})`, width:'100%', filter:'blur(4px)', height:'500px',backgroundRepeat:"no-repeat", backgroundPosition:"center" }}
  >
 
 
